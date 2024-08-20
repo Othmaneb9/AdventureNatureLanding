29402b9c1c5b89b2cdb25937c65b7a3814f40359
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-20 14:26:53 +0200

    ✨ LoadingPage : Add structure

---
+++ src/components/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,39 @@
+export default function LandingPage() {
+  return (
+    <div>
+      <div>
+        <h1>Adventure Voyage Wandering</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Faucibus in
+          libero risus semper habitant arcu eget. Et integer facilisi.
+        </p>
+        <button>Get started</button>
+        <div>
+          <img src="/img/feuille.avif" alt="plante" />
+        </div>
+        <div>
+          <img src="/img/roseblanche.jpg" alt="Rose-Blanche" />
+        </div>
+      </div>
+
+      <div>
+        <h2>Plant . Tree . Nature</h2>
+        <img src="/img/plantefond.jpg" alt="plante" />
+      </div>
+
+      <div>
+        <div>
+          <img src="/img/fleur.png" alt="fleur-blanche" />
+        </div>
+        <div>
+          <img src="/img/botanique.jfif" alt="plante" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit. Faucibus in
+          libero risus semper habitant arcu eget. Et integer facilisi.
+        </p>
+        <h1>Trave Explore Discover</h1>
+      </div>
+    </div>
+  );
+}
