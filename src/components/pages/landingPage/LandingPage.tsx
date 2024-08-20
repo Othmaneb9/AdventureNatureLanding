5471daf4df77bd84da92c61caa5508eb7e7899eb
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-08-20 15:00:20 +0200

    ✨ LandingPage : grid

--- src/components/pages/landingPage/LandingPage.tsx
+++ src/components/pages/landingPage/LandingPage.tsx
@@ -1,32 +1,36 @@
+import styled from "styled-components";
+
 export default function LandingPage() {
   return (
-    <div>
+    <LandingPageStyled>
       <div>
+        <div className="container">
         <h1>Adventure Voyage Wandering</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipiscing elit. Faucibus in
           libero risus semper habitant arcu eget. Et integer facilisi.
         </p>
         <button>Get started</button>
-        <div>
-          <img src="/img/feuille.avif" alt="plante" />
         </div>
         <div>
-          <img src="/img/roseblanche.jpg" alt="Rose-Blanche" />
+        <div className="image">  <img src="/img/feuille.avif" alt="plante" /></div>
+        </div>
+        <div>
+          <div className="image"> <img src="/img/roseblanche.jpg" alt="Rose-Blanche" /></div>
         </div>
       </div>
 
       <div>
         <h2>Plant . Tree . Nature</h2>
-        <img src="/img/plantefond.jpg" alt="plante" />
+        <div className="image"> <img src="/img/plantefond.jpg" alt="plante" /></div>
       </div>
 
       <div>
         <div>
-          <img src="/img/fleur.png" alt="fleur-blanche" />
+          <div className="image"> <img src="/img/fleur.png" alt="fleur-blanche" /></div>
         </div>
         <div>
-          <img src="/img/botanique.jfif" alt="plante" />
+         <div className="image">  <img src="/img/botanique.jfif" alt="plante" /></div>
         </div>
         <p>
           Lorem ipsum dolor sit amet consectetur adipiscing elit. Faucibus in
@@ -34,6 +38,44 @@ export default function LandingPage() {
         </p>
         <h1>Trave Explore Discover</h1>
       </div>
-    </div>
+    </LandingPageStyled>
   );
 }
+
+const LandingPageStyled = styled.div`
+font-family: 'Times';
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+
+h1{
+  width: 280px;
+  font-size: 48px;
+  margin-bottom: 32px;
+}
+p{
+  font-size: 18px;
+  width: 356px;
+  margin-bottom: 32px;
+
+}
+.container{
+  width: 267px;
+  height: 361px;
+}
+button{
+  background: #457D61;
+  color: white;
+  border-radius: 36px;
+  width: 133px;
+  height: 50px;
+  border: none;
+  margin-bottom: 25px;
+
+}
+.image{
+  border-radius: 70px;
+  width: 133px;
+  height: 292px;
+  overflow: hidden;
+}
+`
